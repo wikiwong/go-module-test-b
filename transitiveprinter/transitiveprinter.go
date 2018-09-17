5c28ac9f0154c5cb43c25d90fe304d29a7a3cfed
Rob Wong <rwong@fanatics.com>
2018-09-17 10:40:29 -0600

    update code without bumping version

--- transitiveprinter/transitiveprinter.go
+++ transitiveprinter/transitiveprinter.go
@@ -4,5 +4,5 @@ import "fmt"
 
 // SayHi says hello with a hardcoded version that should be changed whenever the version is incremented
 func SayHi() {
-	fmt.Println("Hi from go-module-test-b! This is version 1")
+	fmt.Println("Hi from go-module-test-b! This is version 1.1")
 }
